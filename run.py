eb292d7f1c74e8d068e0583de46905b41532491a
tenstand <christensmith_@msn.com>
2022-03-28 13:33:24 +0000

    Add player function and create ships for board

--- run.py
+++ run.py
@@ -1,9 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Legend
+# 'X' to place the ship or if it's hit
+# 'O' for a missed shot
+# '.' an empty space
 
-import random
+from random import randint
+
+#Function to create board for computer
+def create_computer_board():
+    board = {'a': 0, 'b': 1, 'c':2, 'd: 3,'e': 4, 'f': 5, 'g': 6, 'h':7}
+
+    ship_locs = random.sample(board.keys(), 8)
+    for loc in ship_locs:
+        board[loc] = 'O'
+    return board
 
 #Function to create board for player
-def create_board():
+def create_player_board():
     board = {'a': 0, 'b': 1, 'c':2, 'd: 3,'e': 4, 'f': 5, 'g': 6, 'h':7}
+
+#Create ships
+def create_ships(board):
+    for ship in range (5):
+        ship_row, ship_column = randint (0,7), randint(0,7)
+        while board [ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint (0,7), randint(0,7)
+        board [ship_row][ship_column] = 'X'
+
+
+
+
+
+    
