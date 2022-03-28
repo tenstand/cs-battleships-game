3e31bc70ab80b0566ea407ef1dde95dc15c1108b
tenstand <christensmith_@msn.com>
2022-03-28 13:08:15 +0000

    Create computer board

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+#Function to create board for player
+def create_board():
+    board = {'a': 0, 'b': 1, 'c':2, 'd: 3,'e': 4, 'f': 5, 'g': 6, 'h':7}
