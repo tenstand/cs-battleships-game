f1c9c1b62026d0f401b19e1364829cd40da95b59
tenstand <christensmith_@msn.com>
2022-03-31 12:05:05 +0000

    Define functions and create valid inputs

--- run.py
+++ run.py
@@ -7,11 +7,12 @@ from random import randint
 
 #Function to create board for computer
 def create_computer_board():
-    board = {'a': 0, 'b': 1, 'c':2, 'd: 3,'e': 4, 'f': 5, 'g': 6, 'h':7}
+    board = {'a': 0, 'b': 1, 'c':2, 'd': 3,'e': 4, 'f': 5, 'g': 6, 'h':7}
+    return board
 
 #Function to create board for player
 def create_player_board():
-    board = {'a': 0, 'b': 1, 'c':2, 'd: 3,'e': 4, 'f': 5, 'g': 6, 'h':7}
+    board = {'a': 0, 'b': 1, 'c':2, 'd': 3,'e': 4, 'f': 5, 'g': 6, 'h':7}
 
 #Create ships
 def create_ships(board):
@@ -36,6 +37,44 @@ def get_ship_location():
     for row in ship_location:
         board[row] = 'O'
     return board
+
+
+def input_is_valid(input, valid_values):
+    try:
+        if int(input) in valid_values:
+            return True
+        else:
+            return False
+    except ValueError:
+        print("The value you entered is not a number, try again")
+        return False
+
+
+def game_menu():
+    print("*******************")
+    print("1 - Instructions")
+    print("2 - Play Game")
+    print("3 - High Scores")
+    print("*******************")
+    option = input("Please enter your choice:  ")
+    valid_values = [1, 2, 3]
+    is_valid = input_is_valid(option, valid_values)
+    if is_valid:
+        if int(option) == 1:
+            print("show instructions")
+        if int(option) == 2:
+            print("show play game")
+        if int(option) == 3:
+            print("show score")
+    else:
+        print("Please select a valid number 1, 2 or 3")
+
+
+if __name__ == "__main__":
+    # print("Running")
+    # pc_board = create_computer_board()
+    # print(pc_board)
+    game_menu()
 
 
 
